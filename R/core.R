f7972c5113fd68bdda77ef0dddcff34ceef565e1
armanazizyan <arman.azizyan@gmail.com>
2026-01-25 18:48:28 -0500

    Initial commit

---
+++ R/core.R
@@ -0,0 +1,148 @@
+library(RSNNS)
+library(foreach)
+library(doParallel)
+
+#' Two-sided moving average smoothing
+#' @param x a numeric vector
+#' @param n number of neighbors to smooth over in total
+#' @param circular if the ends should wrap or remain NA
+#' @return smoothed numeric vector
+#' @export
+ma <- function(x, n = 5, circular = T){
+  stats::filter(x, rep(1 / n, n), sides = 2, circular = circular)
+}
+
+
+#' Fits MLP with defined structure to rolling window of defined size,
+#' Then fits another MLP with double the rolling window,
+#' capturing two windows' uncertainty
+#' @param vec original data
+#' @param act1 activation function of smaller MLP
+#' @param act2 activation function of larger MLP
+#' @param lr1,lr2 learning rates of smaller and larger MLP
+#' @param hl1,hl2 number of neurons in the hidden layers of smaller and larger MLP
+#' @param ep1,ep2 number of training epochs for smaller and larger MLP
+#' @return a list of two elements containing the fitted values for the smaller
+#' and larger MLP models, original input and index.
+#' @export
+fit_mlp <- function(vec, w=100,
+                 act1="Act_TanH_Xdiv2",
+                 act2="Act_TanH_Xdiv2",
+                 lr1=0.01, lr2=0.01, hl1=16,
+                 hl2=32, ep1=1000, ep2=2000){
+  n.val <- length(vec)
+  starts <- 1:(n.val-w)
+  x <- 1:n.val
+  y <- vec
+
+  num_cores <- parallel::detectCores() - 1
+  cl <- makeCluster(num_cores)
+  registerDoParallel(cl)
+  start_time <- Sys.time()
+
+  res.list <- foreach(idx = seq_along(starts), .packages = "RSNNS") %dopar% {
+
+    i <- starts[idx]
+
+    sub_x <- as.matrix(as.vector(scale(x[i:(i + w-1)])))
+    sub_y <- as.matrix(y[i:(i + w-1)])
+
+    net <- mlp(
+      sub_x, sub_y,
+      size = hl1,
+      learnFuncParams = lr1,
+      maxit = ep1,
+      linOut = TRUE,
+      hiddenActFunc = act1 #"Act_TanH_Xdiv2" #
+
+    )
+
+    y_hat <- net$fitted.values
+    cbind(sub_x, y_hat, i:(i + w-1))
+  }
+
+
+  end_time <- Sys.time()
+  time_taken <- end_time - start_time
+  print(time_taken)
+
+
+  #stopCluster(cl)
+
+
+  window_step <- 1
+  starts <- 1:(n.val-w)
+
+  num_cores <- parallel::detectCores() - 1
+  #cl <- makeCluster(num_cores)
+  #registerDoParallel(cl)
+
+
+  start_time <- Sys.time()
+
+  res.list.dbl <- foreach(idx = seq_along(starts), .packages = "RSNNS") %dopar% {
+
+    i <- starts[idx]
+
+
+    sub_x <- as.matrix(as.vector(scale(x[i:(i + w-1)])))
+    sub_y <- as.matrix(y[i:(i + w-1)])
+
+
+    net <- mlp(
+      sub_x, sub_y,
+      size = hl2,
+      learnFuncParams = lr2,
+      maxit = ep2,
+      linOut = TRUE,
+      hiddenActFunc = act2#"Act_TanH_Xdiv2"#
+    )
+
+    y_hat <- net$fitted.values
+
+    cbind(sub_x, y_hat, i:(i + w-1))
+  }
+
+  end_time <- Sys.time()
+  time_taken <- end_time - start_time
+  print(time_taken)
+
+
+  stopCluster(cl)
+
+  return(list(res.list, res.list.dbl))
+}
+
+calc_detector <- function(y, fit_mlp_res, w=100, a=1, b=0){
+  res.list <- fit_mlp_res[[1]]
+  res.list.dbl <- fit_mlp_res[[2]]
+  n.val <- length(y)
+
+  diff1 <- c()
+  diff2 <- c()
+
+
+  for(i in 1:(n.val-2*w)){
+    rss1 <- sum((y[i:(i+w-1)]-res.list[[i]][,2])^2)
+    rss2 <- sum((y[(i+w):(i+2*w-1)]-res.list[[i+w]][,2])^2)
+    rss.tot <- sum((y[i:(i+2*w-1)]-res.list.dbl[[i]][,2])^2)
+    diff1 <- c(diff1,((rss.tot+b)/(rss1+rss2+b)) )
+  }
+
+  for(i in 1:(n.val-2*w)){
+    rss1 <- sum((y[i:(i+w-1)]-res.list[[i]][,2])^2)
+    rss2 <- sum((y[(i+w):(i+2*w-1)]-res.list[[i+w]][,2])^2)
+    rss.tot <- sum((y[i:(i+2*w-1)]-res.list.dbl[[i]][,2])^2)
+    diff2 <- c(diff2,( (rss.tot-rss1-rss2)) )
+  }
+
+  diff2 <- (diff2-min(diff2))/(max(diff2)-min(diff2))
+
+  diff1 <- (diff1-min(diff1))/(max(diff1)-min(diff1))
+
+  diff <- (1-a)*diff1+a*diff2
+
+  diff <- (diff-min(diff))/(max(diff)-min(diff))
+
+  diff
+}
---
+++ R/plotly_mlp.R
@@ -0,0 +1,127 @@
+library(plotly)
+
+
+#' Interactive plot of fit_mlp output
+#' @param y the original vector of observations
+#' @param fit_mlp_res the resulting list of fit_mlp function
+#' @return plotly object
+#' @export
+plot_mlp_slider_3lines <- function(
+    y, fit_mlp_res,
+    t.chp.ind=NA, step = 50, start = 75, w = 100
+) {
+
+  n <- length(y)
+
+  res.list1 <- fit_mlp_res[[1]]
+  res.list2 <- fit_mlp_res[[1]][w+1:length(y)]
+
+  idx_list <- seq(start, n - 2*w, by = step)
+  n_models <- length(idx_list)
+
+  # --- CP segments ---
+  if(!is.na(t.chp.ind)){
+    cp_segments <- lapply(t.chp.ind, function(cp) {
+      list(
+        type = "line",
+        x0 = cp, x1 = cp,
+        y0 = min(y), y1 = max(y),
+        line = list(color = "blue", dash = "dash")
+      )
+    })
+  }else{
+    cp_segments <- NA}
+
+
+  # --- Initial traces (using res.list1, res.list2, res.list3) ---
+  i0 <- idx_list[1]
+
+  p <- plot_ly() %>%
+    # trace 1: original y (static)
+    add_lines(
+      x = 1:n, y = y,
+      name = "Original Signal",
+      line = list(color = "rgba(0,0,0,0.3)")
+    ) %>%
+    # trace 2: model 1 (dynamic)
+    add_lines(
+      x = res.list1[[i0]][,3],
+      y = res.list1[[i0]][,2],
+      name = "Model 1",
+      line = list(color = "red", width = 3)
+    ) %>%
+    # trace 3: model 2 (dynamic)
+    add_lines(
+      x = res.list2[[i0]][,3],
+      y = res.list2[[i0]][,2],
+      name = "Model 2",
+      line = list(color = "green", width = 3)
+    ) %>%
+    # trace 4: model 3 (dynamic)
+    add_lines(
+      x = res.list3[[i0]][,3],
+      y = res.list3[[i0]][,2],
+      name = "Model 3",
+      line = list(color = "purple", width = 3)
+    ) %>%
+    layout(
+      title = "Multiple MLP Fits – Interactive Slider",
+      xaxis = list(title = "Index"),
+      yaxis = list(title = "Value"),
+      shapes = cp_segments,
+
+      sliders = list(
+        list(
+          active = 0,
+          currentvalue = list(prefix = "Model index: "),
+          steps = lapply(1:n_models, function(k) {
+            list(
+              label = as.character(idx_list[k]),
+              method = "animate",
+              args = list(
+                list(paste0("frame", k)),
+                list(mode = "immediate",
+                     frame = list(duration = 0, redraw = FALSE),
+                     transition = list(duration = 0))
+              )
+            )
+          })
+        )
+      )
+    )
+
+
+  # --- Frames: update traces 2, 3, and 4 only ---
+  frames <- lapply(1:n_models, function(k) {
+    i <- idx_list[k]
+
+    list(
+      name = paste0("frame", k),
+      data = list(
+        NULL,  # trace 1 stays unchanged
+        list(  # trace 2 (model 1)
+          x = res.list1[[i]][,3],
+          y = res.list1[[i]][,2],
+          mode = "lines",
+          line = list(color = "red", width = 3)
+        ),
+        list(  # trace 3 (model 2)
+          x = res.list2[[i]][,3],
+          y = res.list2[[i]][,2],
+          mode = "lines",
+          line = list(color = "green", width = 3)
+        ),
+        list(  # trace 4 (model 3)
+          x = res.list3[[i]][,3],
+          y = res.list3[[i]][,2],
+          mode = "lines",
+          line = list(color = "purple", width = 3)
+        )
+      )
+    )
+  })
+
+  p$x$frames <- frames
+  p
+
+}
